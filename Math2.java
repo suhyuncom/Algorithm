aa7693a20f0b079bd4f85262f1b69f49476cbfd4
dhtngus4921 <dhtngus4921@naver.com>
2021-07-13 20:01:21 +0900

    math

--- Math2.java
+++ Math2.java
@@ -1,2 +1,29 @@
-package com.company;public class Math2 {
+package com.company;
+import java.util.*;
+public class Math2 {
+    public int[] solution(int a, int b) {
+        int[] answer = new int[2];
+        int temp=1;
+        int gcd=a*b;
+        while(temp!=0){
+            temp=b%a;
+            b=a;
+            a=temp;
+        }
+        answer[0]=b;
+        answer[1]=gcd/b;
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
+        return answer;
+    }
+    public static void main(String[] args) {
+        int x,y;
+        Scanner sc = new Scanner(System.in);
+        Math2 sol = new Math2();
+
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        sol.solution(x,y);
+    }
 }
